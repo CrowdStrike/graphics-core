bef5bc0bc6a24bb356cdf069898a3300ed06d2bf
zacoppotamus <zacoppotamus@gmail.com>
2023-11-30 14:22:34 +0200

    chore: get tailwind working in test-app

--- test-app/tailwind.config.js
+++ test-app/tailwind.config.js
@@ -1,6 +1,17 @@
 'use strict';
 
+const path = require('path');
+
+const appRoot = __dirname;
+const appEntry = path.join(appRoot, 'app');
+const relevantFilesGlob = '**/*.{html,js,ts,hbs,gjs,gts}';
+
+console.log(
+  path.join(appEntry, relevantFilesGlob)
+)
+
 module.exports = {
+  content: [path.join(appEntry, relevantFilesGlob)],
   presets: [require('@crowdstrike/tailwind-toucan-base')],
 
   plugins: [],
