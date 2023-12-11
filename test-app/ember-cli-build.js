0b4e34f105c1405f85d8306eb5fe405a0e9e1ec7
zacoppotamus <zacoppotamus@gmail.com>
2023-12-11 13:12:48 +0200

    chore: change tailwind configuration

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,49 +1,23 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+// const autoprefixer = require('autoprefixer');
+// const tailwind = require('tailwindcss');
+
+const isProduction = () => EmberApp.env() === 'production';
 
 const {
   configureCSSModules,
   configureTailwind,
 } = require('@crowdstrike/ember-toucan-styles/ember-cli');
-const tailwindConfig = require('./tailwind.config');
 
-// Intimate / Private information:
-// https://github.com/embroider-build/embroider/blob/main/packages/test-setup/src/index.ts#L85
 process.env.EMBROIDER_TEST_SETUP_OPTIONS = 'optimized';
 
 console.debug({ configureTailwind, configureCSSModules });
 
 module.exports = function (defaults) {
-  /**
-   * Sets up some optional environment variables to help out with
-   * both deploy and local debugging
-   *
-   * Start `ember s` with any combination of these:
-   *  - DISABLE_SOURCEMAPS=true
-   *  - DISABLE_UGLIFY=true
-   *  - DISABLE_FINGERPRINTING=true
-   *
-   * Also provides our babel configuration (and consequently, TypeScript support)
-   */
-  // let buildParams = require('ember-cli-deploy-cs/lib/build-params')();
-
-  // buildParams.autoImport.watchDependencies = Object.keys(require('./package').dependencies);
-
   let app = new EmberApp(defaults, {
-    ...configureCSSModules({ tailwindConfig }),
-    /**
-     * If we want, we may diverge the "app name" or import path
-     * from the rootURL (normally these are tied together)
-     *
-     * An example of this is where we'd maybe want to
-     *   import ... from 'falcon-store/...';
-     *   but have the rootURL be: /falcon-store-v2
-     *
-     * Note that if this is manually changed to be different from the package name
-     * the references in both index.html files will also need to be updated
-     *
-     */
+    // ...configureCSSModules({ tailwindConfig }),
     name: 'test-app',
   });
 
@@ -51,13 +25,42 @@ module.exports = function (defaults) {
 
   return maybeEmbroider(app, {
     packagerOptions: {
-      // publicAssetURL: buildParams.fingerprint.prepend,
+      publicAssetURL: '/',
+      cssLoaderOptions: {
+        sourceMap: isProduction() === false,
+        // Native CSS Modules
+        modules: {
+          // global mode, can be either global or local
+          // we set to global mode to avoid hashing tailwind classes
+          mode: 'global',
+          // class naming template
+          localIdentName: isProduction()
+            ? '[sha512:hash:base64:5]'
+            : '[path][name]__[local]',
+        },
+      },
       webpackConfig: {
         output: {
           assetModuleFilename: '[path][name]-[contenthash][ext]',
         },
         module: {
           rules: [
+            {
+              // When webpack sees an import for a CSS files
+              test: /\.css$/i,
+              exclude: /node_modules/,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: isProduction() === false,
+                    postcssOptions: {
+                      config: 'postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
             {
               test: /\.(png|jpg|gif)$/i,
               type: 'asset/resource',
